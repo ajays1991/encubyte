b222a114a4d23701992ddc012b2c357fc7be4649
ajay-wal871 <ajay.sharma@westagilelabs.com>
2024-12-20 00:06:57 +0530

    3- Handle multiple numbers, byebug gem

--- main.rb
+++ main.rb
@@ -1,9 +1,25 @@
+require 'byebug'
 class KataCalculator
     
   def add(input)
     return 0 if input == ''
-    return input.to_i
-  end 
+    @input = input
+    parse_input
+    return sum
+  end
+
+  private
+  def parse_input
+    get_number_array
+  end
+
+  def get_number_array
+    @numbers = @input.split(",").map(&:to_i)
+  end
+
+  def sum
+    @numbers.sum
+  end
 end
 
 
@@ -25,6 +41,12 @@ describe 'KataCalculator' do
     # Test to handle single number
     it "should return the number with a number" do
       expect(@calculator.add("5")).to eql(5)
+    end
+
+    # Test to handle multiple numbers seperated by comma
+    it "should return the sum with 2 or more numbers" do
+      expect(@calculator.add('3,2')).to eql(5)
+      expect(@calculator.add('1,2,3,4,5')).to eql(15)
     end
   end
 end
